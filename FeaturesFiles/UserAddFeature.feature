d1964d51a60719371017ef3a0f9bfccd57901a12
Your Name <mahesh@gmail.com>
2023-04-23 12:50:37 +0530

    Initial commit

---
+++ FeaturesFiles/UserAddFeature.feature
@@ -0,0 +1,8 @@
+Feature: HRM Application
+Scenario: Publishing the article
+        Given URL is up and Running
+        When User Enter Username
+        And User Enter the password
+        And User click on Login Button
+        Then User should be able to see homescreen
+        And User is able to see add employee button.
---
+++ StepDefinitionFile.py
@@ -0,0 +1,36 @@
+from urllib.parse import urljoin
+
+import pytest
+from pytest_bdd import scenario, given, when, then
+
+
+@Given("URL is up and Running")
+def Test():
+    pass
+
+
+@scenario('publish_article.feature', 'Publishing the article')
+def test_publish():
+        pass
+@given("I'm an author user")
+def author_user(auth, author):
+        auth['user'] = author.user
+@given("I have an article", target_fixture="article")
+def article(author):
+    return create_test_article(author=author)
+
+@when("I go to the article page")
+def go_to_article(article, browser):
+    browser.visit(urljoin(browser.url, '/manage/articles/{0}/'.format(article.id)))
+@when("I press the publish button")
+def publish_article(browser):
+    browser.find_by_css('button[name=publish]').first.click()
+@then("I should not see the error message")
+def no_error_message(browser):
+    with pytest.raises(ElementDoesNotExist):
+        browser.find_by_css('.message.error').first
+@then("the article should be published")
+def article_is_published(article):
+    article.refresh() # Refresh the object in the SQLAlchemy session
+    assert article.is_published
+
